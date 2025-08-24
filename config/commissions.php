af9b7ffb94543004913c502783e383200adb2f01
omoritechgod <tguniverse.tech@gmail.com>
2025-08-24 17:32:28 +0100

    commission admin cut

---
+++ config/commissions.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Platform Commission Fees
+    |--------------------------------------------------------------------------
+    |
+    | Define the percentage cut (as a number, not decimal) for each service
+    | category. This allows us to centrally manage all commission rules.
+    |
+    */
+
+    'ride_hailing' => 5,       // 5% of trip fare
+    'food_delivery' => 5,      // 5% of order value
+    'ecommerce' => 5,          // 5% of product price
+    'service_apartments' => 10, // 10% of booking value
+    'auto_maintenance' => 5,   // 5% of service cost
+    'general_services' => 5,   // 5% of service cost
+
+];
